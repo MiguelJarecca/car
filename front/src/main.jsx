53193451794da5ce0a1615d6fcb9769da70c5fa3
miguel <miguel.jarecca@gmail.com>
2024-01-09 09:41:40 -0500

    se trabaja con el deploy

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,13 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+// import { createHashRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
+
+// import { NavBar } from './components/NavBar.jsx'
+// import Product from './components/Product.jsx'
+// import ShopCart from './components/ShopCart.jsx'
+
+// const router = createHashRouter ([
+//   {
+//     path: "/",
+//     Element: <NavBar />,
+//   },
+//   {
+//     path: "/catalog",
+//     element: <Product />,
+//   },
+//   {
+//     path: "/cart",
+//     element: <ShopCart />
+//   }
+// ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <App/>
     </BrowserRouter>
   </React.StrictMode>,
 )
