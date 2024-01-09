a224035ed2565d5cb1fef30c1259f0a0595d7675
miguel <miguel.jarecca@gmail.com>
2024-01-09 12:07:12 -0500

    se cambia a Router Provider

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,18 +1,40 @@
 import './App.css'
-import { Routes, Route, Navigate } from 'react-router-dom'
+import { Navigate, createHashRouter, RouterProvider } from 'react-router-dom'
 import Product from './components/Product'
 import ShopCart from './components/ShopCart';
 import { useItemsCard } from './hooks/useItemsCart';
-import { NavBar } from './components/NavBar';
+// import { NavBar } from './components/NavBar'
 
 function App() {
 
   const {cartItems, handleAddProductCart, handleDeleteProductCart} = useItemsCard();
 
+  const router = createHashRouter ([
+    {
+      path: "/",
+      element: <Navigate to={'catalog'}/>,
+    },
+    {
+      path: "/catalog",
+      element: <Product  handler={handleAddProductCart}/>,
+    },
+    {
+      path: "/cart",
+      element: cartItems?.length <= 0 ?
+        <h2>No hay productos en el carro de compras</h2>
+        :  
+        (
+          <ShopCart cartItems={cartItems} handleDelete={handleDeleteProductCart}/>
+        )
+    }
+  ])
+
   return (
     <div className='container'>
-      
-      <NavBar />
+
+      <RouterProvider router={router}/>
+
+      {/* <NavBar />
 
       <Routes>
         <Route 
@@ -29,7 +51,7 @@ function App() {
             )}
         />
         <Route path='/' element={<Navigate to={'catalog'}/>} />
-      </Routes>
+      </Routes> */}
 
     </div>
   )
--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-// import { createHashRouter, RouterProvider } from 'react-router-dom'
+// import { BrowserRouter } from 'react-router-dom'
+// import { RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
@@ -26,8 +26,8 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    {/* <RouterProvider> */}
       <App/>
-    </BrowserRouter>
+    {/* </RouterProvider> */}
   </React.StrictMode>,
 )
